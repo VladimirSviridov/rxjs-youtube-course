38c2e67fca19f387210de0771a5a7f894ba1b02e
Vladimir Sviridov <vladim.svir@yandex.ru>
2020-06-15 21:53:00 +0300

    после практики

--- src/index.js
+++ src/index.js
@@ -1,52 +1,52 @@
-import {Subject} from 'rxjs'
-import {scan, startWith, shareReplay} from 'rxjs/operators'
+import {Subject} from "rxjs";
+import {scan, startWith, shareReplay} from "rxjs/operators";
 
 const initialState = {
-  counter: 42
+    counter: 0
 }
 
 const pre = document.querySelector('pre')
 
 const handlers = {
-  INCREMENT: state => ({...state, counter: state.counter + 1}),
-  DECREMENT: state => ({...state, counter: state.counter - 1}),
-  ADD: (state, action) => ({...state, counter: state.counter + action.payload}),
-  DEFAULT: state => state
+    INCREMENT: state => ({...state, counter: state.counter + 1}),
+    DECREMENT: state => ({...state, counter: state.counter - 1}),
+    ADD: (state, action) => ({...state, counter: state.counter + action.payload}),
+    DEFAULT: state => state
 }
 
 function reducer(state = initialState, action) {
-  const handler = handlers[action.type] || handlers.DEFAULT
-  return handler(state, action)
+    const handler = handlers[action.type] || handlers.DEFAULT
+    return handler(state, action)
 }
 
 function createStore(rootReducer) {
-  const subj$ = new Subject()
+    const subject$ = new Subject()
 
-  const store$ = subj$.pipe(
-    startWith({type: '__INIT__'}),
-    scan(rootReducer, undefined),
-    shareReplay(1)
-  )
+    const store$ = subject$.pipe(
+        startWith({type: '__INIT__'}),
+        scan(rootReducer, undefined),
+        shareReplay(1)
+    )
 
-  store$.dispatch = action => subj$.next(action)
+    store$.dispatch = action => subject$.next(action)
 
-  return store$
+    return store$
 }
 
 const store$ = createStore(reducer)
 
 store$.subscribe(state => {
-  pre.innerHTML = JSON.stringify(state, null, 2)
-})
+    pre.innerHTML = JSON.stringify(state, null, 2)
+} )
 
 document.getElementById('increment').addEventListener('click', () => {
-  store$.dispatch({type: 'INCREMENT'})
+    store$.dispatch({type: 'INCREMENT'} )
 })
 
 document.getElementById('decrement').addEventListener('click', () => {
-  store$.dispatch({type: 'DECREMENT'})
+    store$.dispatch({type: 'DECREMENT'} )
 })
 
 document.getElementById('add').addEventListener('click', () => {
-  store$.dispatch({type: 'ADD', payload: 10})
+    store$.dispatch({type: 'ADD', payload: 10} )
 })
